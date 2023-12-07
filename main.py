525c4b0037e7292fddf3d8ed0f624e76a188b4ce
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2023-12-07 11:48:02 +0530

    erorr in cors fix

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask,request,jsonify
 from rag_with_palm import RAGPaLMQuery
+from flask_cors import CORS
 
 rag_palm_query_instance = RAGPaLMQuery()
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/chat",methods=["POST"])
+@app.route("/chat", methods=["POST"])
 def chat():
     if request.method == "POST":
-        data = request.get_json()
-        k = data.get("respon")
+        data = request.get_json(force=True)  # Added 'force=True'
+        k = data.get("message")
         respons = rag_palm_query_instance.query_response(k)
         res = {
-            "msg":str(respons)
+            "msg": str(respons)
         }
-        #print(res.get("msg"))
-        return jsonify(res),200
+        return jsonify(res), 200
+
 
 
 if __name__ == "__main__":
